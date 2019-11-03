86a099d5de62b9dab52d9fc36cc7cdba1b3fbc66
rampeled <dudyrp@gmail.com>
2019-11-03 13:19:42 +0200

    tip calculator first commit

---
+++ TipCalculator_js.js
@@ -0,0 +1,3 @@
+var Bill_amount = document.getElementById('Bill_amount');
+var service_satisfaction = document.getElementById('service_satisfaction');
+var people_amount = document.getElementById('people_amount');
